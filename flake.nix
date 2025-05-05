830577009d13973242881e45f637bfd21e41fb2a
Paolo Brasolin <paolo.brasolin@gmail.com>
2025-05-05 17:25:57 +0200

    #2 Configure proper nix build

--- flake.nix
+++ flake.nix
@@ -8,10 +8,7 @@
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
-        pkgs = import nixpkgs {
-          inherit system;
-          config = {};
-        };
+        pkgs = import nixpkgs {inherit system;};
       in {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
@@ -21,6 +18,31 @@
             # NOTE: this flags avoids ERR_OSSL_EVP_UNSUPPORTED
             export NODE_OPTIONS=--openssl-legacy-provider
           '';
+        };
+        packages = rec {
+          nodeModules = pkgs.mkYarnModules {
+            pname = "node-modules";
+            version = "0.1.0";
+            packageJSON = ./package.json;
+            yarnLock = ./yarn.lock;
+          };
+          frontend = pkgs.stdenv.mkDerivation {
+            name = "frontend";
+            src = ./.;
+            buildInputs = [pkgs.yarn];
+            configurePhase = ''
+              ln -s ${nodeModules}/node_modules node_modules
+              # NOTE: this flags avoids ERR_OSSL_EVP_UNSUPPORTED
+              export NODE_OPTIONS=--openssl-legacy-provider
+            '';
+            buildPhase = ''
+              yarn build
+            '';
+            installPhase = ''
+              cp -r build $out
+            '';
+          };
+          default = frontend;
         };
       }
     );
